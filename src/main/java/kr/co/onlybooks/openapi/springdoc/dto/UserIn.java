a29358cec76791d5d32d68abdb19599c898ec822
Homo Efficio <homo.efficio@gmail.com>
2023-12-10 17:20:13 +0900

    dto에 스키마 설명 추가

--- src/main/java/kr/co/onlybooks/openapi/springdoc/dto/UserIn.java
+++ src/main/java/kr/co/onlybooks/openapi/springdoc/dto/UserIn.java
@@ -1,14 +1,37 @@
 package kr.co.onlybooks.openapi.springdoc.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import kr.co.onlybooks.openapi.springdoc.model.User;
 
 import java.time.LocalDate;
 
 public record UserIn(
+
+        @Schema(description = "이메일 주소")
+        @NotNull
+        @Email
+        @Size(max = 50)
         String email,
+
+        @Schema(description = "비밀번호")
+        @NotNull
+        @Size(min = 8, max = 20)
         String password,
+
+        @Schema(description = "이름")
+        @NotNull
+        @Size(max = 30)
         String firstName,
+
+        @Schema(description = "성")
+        @NotNull
+        @Size(max = 30)
         String lastName,
+
+        @Schema(description = "생년월일 yyyy-MM-dd")
         LocalDate birthDate
 ) {
     public User toEntity(String id) {
--- src/main/java/kr/co/onlybooks/openapi/springdoc/dto/UserOut.java
+++ src/main/java/kr/co/onlybooks/openapi/springdoc/dto/UserOut.java
@@ -1,13 +1,22 @@
 package kr.co.onlybooks.openapi.springdoc.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import kr.co.onlybooks.openapi.springdoc.model.User;
 
 import java.time.LocalDate;
 
 public record UserOut(
+
+        @Schema(description = "사용자 식별자")
         String id,
+
+        @Schema(description = "이메일 주소")
         String email,
+
+        @Schema(description = "전체 이름")
         String fullName,
+
+        @Schema(description = "생년월일 yyyy-MM-dd")
         LocalDate birthDate
 ) {
     public static UserOut fromEntity(User user) {
