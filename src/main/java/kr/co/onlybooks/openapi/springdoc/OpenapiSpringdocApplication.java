f9a3fdea64ac3c902e507a024db78dbed6102fad
Homo Efficio <homo.efficio@gmail.com>
2023-12-10 16:24:17 +0900

    Initial Commit

---
+++ src/main/java/kr/co/onlybooks/openapi/springdoc/OpenapiSpringdocApplication.java
@@ -0,0 +1,13 @@
+package kr.co.onlybooks.openapi.springdoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenapiSpringdocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenapiSpringdocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/onlybooks/openapi/springdoc/OpenapiSpringdocApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.onlybooks.openapi.springdoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenapiSpringdocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
